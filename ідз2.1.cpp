8bc505fc3894e0409f1d25d3e5661f7275de27de
ShchurovIKM725a <Bohdan.Shchurov@infiz.khpi.edu.ua>
2026-03-23 10:00:08 +0200

    Add files via upload

---
+++ ідз2.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<double>> matrix(n, vector<double>(n));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    double sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (matrix[i][i] < 0) {
+            sum += matrix[i][i];
+            count++;
+        }
+    }
+
+    if (count > 0) {
+        cout << sum / count << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+}
