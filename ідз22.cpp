01ea2a85568787009f77b037bd32e65bb4ad994e
ShchurovIKM725a <Bohdan.Shchurov@infiz.khpi.edu.ua>
2026-03-23 10:02:02 +0200

    Add files via upload

---
+++ ідз22.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<double>> matrix(n, vector<double>(n));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    double threshold;
+    cin >> threshold;
+
+    int rowsCount = 0;
+
+    for (int i = 0; i < n; ++i) {
+        double rowSum = 0;
+
+        for (int j = 0; j < n; ++j) {
+            rowSum += matrix[i][j];
+        }
+
+        if (n != 0 && (rowSum / n) < threshold) {
+            rowsCount++;
+        }
+    }
+
+    cout << rowsCount << endl;
+}
